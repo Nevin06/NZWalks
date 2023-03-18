e56a88eb70dd04ade736b7985d28e1578aafbfb9
Nevin06 <thomasnevink@gmail.com>
2023-03-18 13:14:37 +0530

    End Of Section 2

---
+++ NZWalks/NZWalks.API/Data/NZWalksDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Models.Domain;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace NZWalks.API.Data
+{
+    public class NZWalksDbContext : DbContext
+    {
+        public NZWalksDbContext(DbContextOptions<NZWalksDbContext> options) : base(options)
+        {
+        }
+
+        // use the NZDbContext so that it can query or persist data to the regions table.
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+    }
+}
