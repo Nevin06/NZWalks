744d267b47f5bcd02090a1cb38465ebefaaf2fd9
Nevin06 <thomasnevink@gmail.com>
2023-03-18 16:20:49 +0530

    End Of Section 3

---
+++ NZWalks/NZWalks.API/Profiles/RegionProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+
+namespace NZWalks.API.Profiles
+{
+    public class RegionProfile : Profile
+    {
+        public RegionProfile() {
+            CreateMap<Models.Domain.Region, Models.DTO.Region>()
+                .ReverseMap();
+                //.ForMember(dest => dest.Id, options => options.MapFrom(src => src.RegionId));
+        }
+    }
+}
